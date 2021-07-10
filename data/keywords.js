e06311c37bf3db08de2ddacbe8f3c68a2ad5912c
KUMAR Vipin <vipin.kumar2@soprasteria.com>
2021-07-10 11:01:31 +0530

    Initial version

---
+++ data/keywords.js
@@ -0,0 +1,163 @@
+const data = [
+    {
+      "title": "Old Man's War",
+      "description": {
+        "firstName": "John",
+        "lastName": "Scalzi"
+      }
+    },
+    {
+      "title": "The Lock Artist",
+      "description": {
+        "firstName": "Steve",
+        "lastName": "Hamilton"
+      }
+    },
+    {
+      "title": "HTML5",
+      "description": {
+        "firstName": "Remy",
+        "lastName": "Sharp"
+      }
+    },
+    {
+      "title": "Right Ho Jeeves",
+      "description": {
+        "firstName": "P.D",
+        "lastName": "Woodhouse"
+      }
+    },
+    {
+      "title": "The Code of the Wooster",
+      "description": {
+        "firstName": "P.D",
+        "lastName": "Woodhouse"
+      }
+    },
+    {
+      "title": "Thank You Jeeves",
+      "description": {
+        "firstName": "P.D",
+        "lastName": "Woodhouse"
+      }
+    },
+    {
+      "title": "The DaVinci Code",
+      "description": {
+        "firstName": "Dan",
+        "lastName": "Brown"
+      }
+    },
+    {
+      "title": "Angels & Demons",
+      "description": {
+        "firstName": "Dan",
+        "lastName": "Brown"
+      }
+    },
+    {
+      "title": "The Silmarillion",
+      "description": {
+        "firstName": "J.R.R",
+        "lastName": "Tolkien"
+      }
+    },
+    {
+      "title": "Syrup",
+      "description": {
+        "firstName": "Max",
+        "lastName": "Barry"
+      }
+    },
+    {
+      "title": "The Lost Symbol",
+      "description": {
+        "firstName": "Dan",
+        "lastName": "Brown"
+      }
+    },
+    {
+      "title": "The Book of Lies",
+      "description": {
+        "firstName": "Brad",
+        "lastName": "Meltzer"
+      }
+    },
+    {
+      "title": "Lamb",
+      "description": {
+        "firstName": "Christopher",
+        "lastName": "Moore"
+      }
+    },
+    {
+      "title": "Fool",
+      "description": {
+        "firstName": "Christopher",
+        "lastName": "Moore"
+      }
+    },
+    {
+      "title": "Incompetence",
+      "description": {
+        "firstName": "Rob",
+        "lastName": "Grant"
+      }
+    },
+    {
+      "title": "Fat",
+      "description": {
+        "firstName": "Rob",
+        "lastName": "Grant"
+      }
+    },
+    {
+      "title": "Colony",
+      "description": {
+        "firstName": "Rob",
+        "lastName": "Grant"
+      }
+    },
+    {
+      "title": "Backwards, Red Dwarf",
+      "description": {
+        "firstName": "Rob",
+        "lastName": "Grant"
+      }
+    },
+    {
+      "title": "The Grand Design",
+      "description": {
+        "firstName": "Stephen",
+        "lastName": "Hawking"
+      }
+    },
+    {
+      "title": "The Book of Samson",
+      "description": {
+        "firstName": "David",
+        "lastName": "Maine"
+      }
+    },
+    {
+      "title": "The Preservationist",
+      "description": {
+        "firstName": "David",
+        "lastName": "Maine"
+      }
+    },
+    {
+      "title": "Fallen",
+      "description": {
+        "firstName": "David",
+        "lastName": "Maine"
+      }
+    },
+    {
+      "title": "Monster 1959",
+      "description": {
+        "firstName": "David",
+        "lastName": "Maine"
+      }
+    }
+  ]
---
+++ index.js
@@ -0,0 +1,94 @@
+const options = {
+  includeScore: true,
+  includeMatches: true,
+  useExtendedSearch: false,
+  threshold: 0.6,
+  keys: [
+    { name: "title", weight: 3 },
+    { name: "description.firstName", weight: 2 },
+    { name: "description.lastName", weight: 1 }
+  ],
+};
+
+var allSearchKeywords = data;
+
+clearSearch = () => {
+  console.log("clear");
+  document.getElementById("textSearch").value = "";
+  document.getElementById("results").innerHTML = "";
+};
+
+search = () => {
+  let text = document.getElementById("textSearch").value;
+  const fuse = new Fuse(allSearchKeywords, options);
+  const res = fuse.search(text);
+  //console.log("Search results>>>>>", res)
+  const finalSearchResults = sortByWeight(res, text); // custom function for sort the resuly by keywords
+  var html = "";
+  if (res.length) {
+    html += `<h4><b>${res.length} search result(s) found</b></h4>`;
+    for (let i = 0; i < res.length; i++) {
+      html += `<div class="searches"><p><b>Title: </b>${finalSearchResults[i].item.title}</p><p><b>First name: </b>${finalSearchResults[i].item.description.firstName} , <b>Last name: </b>${finalSearchResults[i].item.description.lastName}</p></div>`;
+    }
+    document.getElementById("results").innerHTML = html;
+  } else {
+    document.getElementById("results").innerHTML =
+      "<h4> <b>No result found </b></h4>";
+  }
+};
+
+sortByWeight = (searchRes, searchKeyword) => {
+  searchRes.forEach((element) => {
+    element.matches.forEach((match) => {
+      if (
+        match.key === "keywordsWithWeight.keyword" &&
+        (match.value.indexOf(searchKeyword.toLowerCase()) > -1 ||
+          searchKeyword.indexOf(match.value.toLowerCase()) > -1)
+      ) {
+        element.isMatchKeyword = true;
+      }
+    });
+  });
+  // console.log("searchRes>>>KEYWORDS", searchRes);
+  searchRes.forEach((element) => {
+    if (element.isMatchKeyword) {
+      element.item.keywordsWithWeight.forEach((item) => {
+        if (
+          item.keyword.toLowerCase().indexOf(searchKeyword.toLowerCase()) >
+            -1 ||
+          searchKeyword.toLowerCase().indexOf(item.keyword.toLowerCase()) > -1
+        ) {
+          element.searchKeywordWeight = item.weight;
+        }
+      });
+    }
+  });
+  //console.log("searchRes>>>KEYWORDS", searchRes);
+  searchRes.sort((a, b) => {
+    return b.searchKeywordWeight - a.searchKeywordWeight;
+  });
+  console.log("FINAL>>>", searchRes);
+  return searchRes;
+};
+
+debounce = (cb, interval, immediate) => {
+  var timeout;
+
+  return function () {
+    var context = this,
+      args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) cb.apply(context, args);
+    };
+
+    var callNow = immediate && !timeout;
+
+    clearTimeout(timeout);
+    timeout = setTimeout(later, interval);
+
+    if (callNow) cb.apply(context, args);
+  };
+};
+
+document.getElementById("textSearch").onkeyup = debounce(search, 300);
